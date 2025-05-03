00a13b38ba9fc5ee062de526748542cc84609a24
Jian James Astrero <jian.astrero@firstdigitalfinance.com>
2025-05-03 23:05:38 +0800

    Update LogLevel enum to include integer levels
    Added integer values to the LogLevel enum to establish an explicit hierarchy of log levels. This change enables easier comparison and sorting of log levels based on their severity.

--- utils/src/commonMain/kotlin/dev/jianastrero/utils/log/LogLevel.kt
+++ utils/src/commonMain/kotlin/dev/jianastrero/utils/log/LogLevel.kt
@@ -1,8 +1,8 @@
 package dev.jianastrero.utils.log
 
-enum class LogLevel {
-    ERROR,
-    WARNING,
-    INFO,
-    DEBUG
+enum class LogLevel(val level: Int) {
+    DEBUG(0),
+    INFO(1),
+    WARNING(2),
+    ERROR(3),
 }
