898c2997ddd2a7c97b77267045ad9eca3626f3e3
Jian James Astrero <jian.astrero@firstdigitalfinance.com>
2025-05-03 23:05:50 +0800

    Add enhanced logging capabilities to LogUtil
    Introduced `logDeep` method for detailed logging with class properties and call stacks. Refactored default tags and levels to use `LogUtil` constants for better configurability. Improved error handling and logging consistency across utility functions.

--- utils/src/commonMain/kotlin/dev/jianastrero/utils/println.kt
+++ utils/src/commonMain/kotlin/dev/jianastrero/utils/println.kt
@@ -1,5 +1,6 @@
 package dev.jianastrero.utils
 
 import dev.jianastrero.utils.log.LogLevel
+import dev.jianastrero.utils.log.LogUtil
 
-expect fun println(message: String, tag: String = "JIANDDEBUG", level: LogLevel = LogLevel.DEBUG)
+expect fun println(message: String, tag: String = LogUtil.tag, level: LogLevel = LogLevel.DEBUG)
