eff0e25f9c33396cb65d2c2ba07596473c3faa66
Jian James Astrero <jianjamesastrero@gmail.com>
2025-05-03 23:05:50 +0800

    Add enhanced logging capabilities to LogUtil
    Introduced `logDeep` method for detailed logging with class properties and call stacks. Refactored default tags and levels to use `LogUtil` constants for better configurability. Improved error handling and logging consistency across utility functions.

--- utils/src/commonMain/kotlin/dev/jianastrero/utils/log/LogUtil.kt
+++ utils/src/commonMain/kotlin/dev/jianastrero/utils/log/LogUtil.kt
@@ -1,23 +1,85 @@
 package dev.jianastrero.utils.log
 
 import dev.jianastrero.utils.println
+import dev.jianastrero.utils.reflect.getProperties
+import dev.jianastrero.utils.trace.getCaller
+import dev.jianastrero.utils.trace.getStackTrace
 
-inline fun <reified T> T.log(
-    tag: String = "JIANDDEBUG",
+data object LogUtil {
+    private const val DEFAULT_TAG = "JIANDDEBUG"
+
+    internal const val FILE_NAME = "LogUtil.kt"
+    internal const val METHOD_NAME = "logDeep"
+
+    var minLogLevel: LogLevel = LogLevel.DEBUG
+    val tag: String = DEFAULT_TAG
+}
+
+fun <T> T.log(
+    tag: String = LogUtil.tag,
     level: LogLevel = LogLevel.DEBUG,
 ): T {
+    if (level.level < LogUtil.minLogLevel.level) return this
+    if (this == null) {
+        println("$this is NULL", level = level)
+        return this
+    }
+
     if (this is Throwable) {
-        val message = "(${T::class.qualifiedName}) ${this.message ?: "No ERROR message"}"
+        val message = "(${this::class.qualifiedName}) ${this.message ?: "No ERROR message"}"
         println(message = message, tag = tag, level = LogLevel.ERROR)
         println(stackTraceToString(), tag = tag, level = LogLevel.ERROR)
         return this
     }
 
-    val logMessage = "(${T::class.qualifiedName}) $this"
+    val logMessage = "(${this::class.qualifiedName}) $this"
     println(message = logMessage, tag = tag, level = level)
     return this
 }
 
-inline fun <reified T> T.logStackTrace() {
+fun <T> T.logDeep(name: String, tag: String = LogUtil.tag, level: LogLevel = LogLevel.DEBUG): T {
+    if (level.level < LogUtil.minLogLevel.level) return this
+    if (this == null) {
+        "$name is NULL".log(tag = tag, level = level)
+        return this
+    }
 
+    val kClass = this::class
+    val caller = getCaller()
+    val className = kClass.qualifiedName ?: "Unknown Class"
+    val properties = runCatching { this.getProperties() }
+        .getOrElse { exception ->
+            exception.log(name, level)
+            null
+        }
+    val callStack = runCatching { getStackTrace() }
+        .getOrElse { exception ->
+            exception.log(name, level)
+            null
+        }
+
+    val logMessage = StringBuilder()
+        .appendLine()
+        .appendLine(caller)
+        .appendLine("| $name ($className)")
+        .apply {
+            properties?.forEach { (propertyName, property) ->
+                val (propertyClass, propertyValue) = property
+                appendLine("⌙ $propertyName ($propertyClass) | $propertyValue")
+            }
+        }
+        .appendLine("Call Stack:")
+        .apply {
+            callStack?.forEachIndexed { index, stack ->
+                appendLine("${" ".repeat(index + 1)}⌙ $stack")
+            }
+        }
+        .toString()
+    println(
+        message = logMessage,
+        tag = tag,
+        level = level,
+    )
+
+    return this
 }
