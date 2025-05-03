23a260dfabc6304997c340e6a13d9d5b9a566641
Jian James Astrero <jian.astrero@firstdigitalfinance.com>
2025-05-03 23:04:40 +0800

    Add platform-specific implementations for getProperties function
    Introduces the `getProperties` function with platform-specific implementations for Android and JVM. The function retrieves a map of property names, types, and values, while on Native platforms it throws an `IllegalStateException` as it is not supported. An `expect` declaration is included in the common code to handle platform-specific behavior.

---
+++ utils/src/jvmMain/kotlin/dev/jianastrero/utils/reflect/Reflect.jvm.kt
@@ -0,0 +1,18 @@
+package dev.jianastrero.utils.reflect
+
+import kotlin.Throws
+import kotlin.reflect.full.memberProperties
+
+@Throws(IllegalStateException::class)
+actual fun <T : Any> T.getProperties(): Map<String, Pair<String, Any?>> {
+    val kClass = this::class
+    val properties = kClass.memberProperties
+        .filter { it.name != "Companion" }
+        .associate { property ->
+            val name = property.name
+            val value = property.getter.call(this)
+            val valueClass = value?.let { it::class.qualifiedName } ?: "Unknown Class"
+            name to (valueClass to value)
+        }
+    return properties
+}
