883c20b642abf9c6b800655734f52234a0fe999f
Jian James Astrero <jianjamesastrero@gmail.com>
2025-06-10 01:01:46 +0800

    Refactor `truthy` extension functions for improved clarity and performance
    Replaced the single `truthy()` function with optimized and type-specific inline extension functions. This enhances readability, promotes better type safety, and improves performance by reducing cyclomatic complexity.

--- utils/src/commonMain/kotlin/dev/jianastrero/utils/ext/Nullity.kt
+++ utils/src/commonMain/kotlin/dev/jianastrero/utils/ext/Nullity.kt
@@ -4,36 +4,23 @@
 
 package dev.jianastrero.utils.ext
 
-@Suppress("CyclomaticComplexMethod")
-fun <T> T?.truthy(): Boolean {
-    return when (this) {
-        null -> false
-        is Boolean -> this
-
-        is Char -> this != '\u0000'
-        is String -> isNotEmpty()
-        is CharSequence -> isNotEmpty()
-
-        is Float -> !isNaN() && this != 0f
-        is Double -> !isNaN() && this != 0.0
-        is Number -> this != 0
-
-        is Collection<*> -> isNotEmpty()
-        is Map<*, *> -> isNotEmpty()
-        is Sequence<*> -> iterator().hasNext()
-        is Iterable<*> -> iterator().hasNext()
-
-        is Array<*> -> isNotEmpty()
-        is BooleanArray -> isNotEmpty()
-        is ByteArray -> isNotEmpty()
-        is CharArray -> isNotEmpty()
-        is ShortArray -> isNotEmpty()
-        is IntArray -> isNotEmpty()
-        is FloatArray -> isNotEmpty()
-        is DoubleArray -> isNotEmpty()
-
-        is ClosedRange<*> -> !isEmpty()
-        is OpenEndRange<*> -> !isEmpty()
-        else -> true
-    }
-}
+inline fun String?.truthy(): Boolean = this != null
+inline fun Boolean?.truthy(): Boolean = this == true
+inline fun Char?.truthy(): Boolean = this != '\u0000'
+inline fun Float?.truthy(): Boolean = this != null && !isNaN() && this != 0f
+inline fun Double?.truthy(): Boolean = this != null && !isNaN() && this != 0.0
+inline fun Number?.truthy(): Boolean = this != null && this != 0.0
+inline fun Collection<*>.truthy(): Boolean = isNotEmpty()
+inline fun Map<*, *>.truthy(): Boolean = isNotEmpty()
+inline fun Sequence<*>.truthy(): Boolean = iterator().hasNext()
+inline fun Iterable<*>.truthy(): Boolean = iterator().hasNext()
+inline fun Array<*>.truthy(): Boolean = isNotEmpty()
+inline fun BooleanArray.truthy(): Boolean = isNotEmpty()
+inline fun ByteArray.truthy(): Boolean = isNotEmpty()
+inline fun CharArray.truthy(): Boolean = isNotEmpty()
+inline fun ShortArray.truthy(): Boolean = isNotEmpty()
+inline fun IntArray.truthy(): Boolean = isNotEmpty()
+inline fun FloatArray.truthy(): Boolean = isNotEmpty()
+inline fun DoubleArray.truthy(): Boolean = isNotEmpty()
+inline fun ClosedRange<*>.truthy(): Boolean = !isEmpty()
+inline fun OpenEndRange<*>.truthy(): Boolean = !isEmpty()
